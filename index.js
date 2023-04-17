2bd7d6a8ad42a486ea9102d10ea41c7acea9124e
trueToastedCode <lennard2hp@gmail.com>
2023-04-17 13:30:51 +0200

    fix dependencies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Id from '../Id'
 import Hash from '../Hash'
+import isValidDate from '../is-valid-date'
 
 import buildMakePassword from './password'
 
-export default buildMakePassword({ Id, Hash })
+export default buildMakePassword({ Id, Hash, isValidDate })
