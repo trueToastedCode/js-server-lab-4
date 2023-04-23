f32f51d6f0b4efe5f3050e3679fad0d80bf89a82
trueToastedCode <lennard2hp@gmail.com>
2023-04-23 18:14:18 +0200

    update imports

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import jwt from 'jsonwebtoken'
+
 import Id from '../Id'
 import isValidDate from '../is-valid-date'
 
 import buildMakeSession from './session'
 
-export default buildMakeSession({ Id, isValidDate })
+export default buildMakeSession({ jwt, Id, isValidDate })
