61e4c783cc4b0df0b85df989656f96e1e108940f
trueToastedCode <lennard2hp@gmail.com>
2023-04-23 19:36:57 +0200

    merge Token update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import Token from '../Token'
 import Id from '../Id'
 import isValidDate from '../is-valid-date'
 
 import buildMakeSession from './session'
 
-export default buildMakeSession({ Token, Id, isValidDate })
+export default makeBuildMakeSession({ Id, isValidDate })
--- session.js
+++ session.js
@@ -1,51 +1,52 @@
-export default function buildMakeSession ({ Token, Id, isValidDate }) {
-  return function makeSession ({
-    id = Id.createId(),
-    userId,
-    createdOn = Date.now(),
-    lastActiveOn = Date.now()
-  } = {}) {
-    if (!Id.isValidId(id)) {
-      throw new Error('Invalid id')
-    }
-
-    if (!Id.isValidId(userId)) {
-      throw new Error('Invalid user id')
-    }
-
-    if (!isValidDate(createdOn)) {
-      throw new Error('Invalid created on date')
-    }
-
-    if (!isValidDate(lastActiveOn)) {
-      throw new Error('Invalid last active on date')
-    }
-
-    const now = Date.now()
-    const msSinceLastActive = now - lastActiveOn
-    lastActiveOn = now
-
-    if (msSinceLastActive > 1.8e+6) {
-      // more than 30 minutes
-      throw new Error('Session inactive for more than 30 minutes')
-    }
-
-    if (lastActiveOn - createdOn > 8.64e+7) {
-      // session older than one day
-      throw new Error('Session max age of one day exceeded')
-    }
-
-    return Object.freeze({
-      getId: () => id,
-      getUserId: () => userId,
-      getCreatedOn: () => createdOn,
-      getLastActiveOn: () => lastActiveOn,
-      getExpireAt: () => Math.min(lastActiveOn + 1.8e+6, createdOn + 8.64e+7),
-      getToken: (secret) => Token.sign({
-        expireAt: Math.min(lastActiveOn + 1.8e+6, createdOn + 8.64e+7),
-        secret,
-        data: { id }
+export default function makeBuildMakeSession ({ Id, isValidDate }) {
+  return function buildMakeSession ({ Token }) {
+    return function makeSession ({
+      id = Id.createId(),
+      userId,
+      createdOn = Date.now(),
+      lastActiveOn = Date.now()
+    } = {}) {
+      if (!Id.isValidId(id)) {
+        throw new Error('Invalid id')
+      }
+  
+      if (!Id.isValidId(userId)) {
+        throw new Error('Invalid user id')
+      }
+  
+      if (!isValidDate(createdOn)) {
+        throw new Error('Invalid created on date')
+      }
+  
+      if (!isValidDate(lastActiveOn)) {
+        throw new Error('Invalid last active on date')
+      }
+  
+      const now = Date.now()
+      const msSinceLastActive = now - lastActiveOn
+      lastActiveOn = now
+  
+      if (msSinceLastActive > 1.8e+6) {
+        // more than 30 minutes
+        throw new Error('Session inactive for more than 30 minutes')
+      }
+  
+      if (lastActiveOn - createdOn > 8.64e+7) {
+        // session older than one day
+        throw new Error('Session max age of one day exceeded')
+      }
+  
+      return Object.freeze({
+        getId: () => id,
+        getUserId: () => userId,
+        getCreatedOn: () => createdOn,
+        getLastActiveOn: () => lastActiveOn,
+        getExpireAt: () => Math.min(lastActiveOn + 1.8e+6, createdOn + 8.64e+7),
+        getToken: () => Token.sign({
+          expireAt: Math.min(lastActiveOn + 1.8e+6, createdOn + 8.64e+7),
+          data: { id }
+        })
       })
-    })
+    }
   }
 }
