bfccd48a5af5e6e0e7ea019b876fbf04ce42b518
trueToastedCode <lennard2hp@gmail.com>
2023-05-11 11:59:29 +0200

    rm import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import makeBalancedAmqpAccess from '../balanced-amqp-access'
 import defaultAmqpSend from '../default-amqp-send'
-import parseEnvList from '../parse-env-list'
 
 import makeMsPasswordApiAccess from './ms-username-api-access'
 
-export default makeMsPasswordApiAccess({ makeBalancedAmqpAccess, defaultAmqpSend, parseEnvList })
+export default makeMsPasswordApiAccess({ makeBalancedAmqpAccess, defaultAmqpSend })
