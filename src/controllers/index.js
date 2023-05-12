069fe117a441f803bf492ad81dea7b15428fed71
trueToastedCode <lennard2hp@gmail.com>
2023-05-12 11:46:16 +0200

    fix stuff

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,7 +4,7 @@ import currentService from '../use-cases'
 
 const testAddPassword = makeDefaultController({ ucFunc: currentService.testAddPassword })
 const removePassword = makeDefaultController({ ucFunc: currentService.removePassword })
-const addPassword = makeDefaultController({ ucFunc: currentService.addPassword })
+const addPassword = makeDefaultController({ ucFunc: currentService.addPassword, statusCode: 201 })
 const changePassword = makeDefaultController({ ucFunc: currentService.changePassword })
 const verifyPassword = makeDefaultController({ ucFunc: currentService.verifyPassword })
 const findPassword = makeDefaultController({ ucFunc: currentService.findPassword })
