b826b2630b6389c5fa49f9d94263c68cc5a13980
trueToastedCode <lennard2hp@gmail.com>
2023-05-13 19:21:20 +0200

    make changeUsername more secure

--- src/use-cases/change-username.js
+++ src/use-cases/change-username.js
@@ -7,36 +7,33 @@ export default function makeChangeUsername ({ currentDb, currentCache, findUsern
     }
     const findResult = await findUsername({ id, userId, username })
     const usernameEntity = makeUsername({ ...findResult, modifiedOn: undefined, username: newUsername })
-    const results = await Promise.all([
-      currentCache.removeUsername({
-        id: usernameEntity.getId()
-      }),
-      currentDb.updateOne({
-        id: usernameEntity.getId(),
-        username: usernameEntity.getUsername(),
-        modifiedOn: usernameEntity.getModifiedOn()
-      })
-    ])
-    if (results[1] == null) {
-      throw new Error('No db result')
-    }
-    if (results[0] == null) {
+    const cacheRmResult = await currentCache.removeUsername({ id: usernameEntity.getId() })
+    if (cacheRmResult == null) {
       throw new Error('No cache result')
     }
-    if (results[0]) {
-      const setCacheResult = await currentCache.setUsername({
-        info: {
-          id: usernameEntity.getId(),
-          userId: usernameEntity.getUserId(),
-          username: usernameEntity.getUsername(),
-          modifiedOn: usernameEntity.getModifiedOn()
-        },
-        timeLeftS: process.env.USERNAME_CACHE_S
-      })
-      if (setCacheResult == null) {
-        throw new Error('No cache result')
-      }
+    const dbUpdateResult = await currentDb.updateOne({
+      id: usernameEntity.getId(),
+      username: usernameEntity.getUsername(),
+      modifiedOn: usernameEntity.getModifiedOn()
+    })
+    if (dbUpdateResult == null) {
+      throw new Error('No db result')
     }
-    return results[1]
+    if (cacheRmResult === false) {
+      return dbUpdateResult
+    }
+    const cacheSetResult = await currentCache.setUsername({
+      info: {
+        id: usernameEntity.getId(),
+        userId: usernameEntity.getUserId(),
+        username: usernameEntity.getUsername(),
+        modifiedOn: usernameEntity.getModifiedOn()
+      },
+      timeLeftS: process.env.USERNAME_CACHE_S
+    })
+    if (cacheSetResult == null) {
+      throw new Error('No cache result')
+    }
+    return dbUpdateResult
   }
 }
