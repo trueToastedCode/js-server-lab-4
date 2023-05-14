252e512c320c9c40d50cffd706f677554d0c8e71
trueToastedCode <lennard2hp@gmail.com>
2023-05-14 11:26:22 +0200

    make removeUsername more secure

--- src/use-cases/remove-username.js
+++ src/use-cases/remove-username.js
@@ -1,15 +1,13 @@
 export default function makeRemoveUsername ({ currentDb, currentCache }) {
   return async function removeUsername (info) {
-    const results = await Promise.all([
-      currentDb.removeUsername(info),
-      currentCache.removeUsername(info)
-    ])
-    if (results[0] == null) {
-      throw new Error('No db result')
-    }
-    if (results[1] == null) {
+    const cacheResult = await currentCache.removeUsername(info)
+    if (cacheResult == null) {
       throw new Error('No cache result')
     }
-    return results[0]
+    const dbResult = await currentDb.removeUsername(info)
+    if (dbResult == null) {
+      throw new Error('No db result')
+    }
+    return dbResult
   }
 }
